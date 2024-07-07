feb1b3491af8e6e877c0f36981a91ae011e8bfcf
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-07 19:33:09 +0000

    comparsion

---
+++ .devcontainer/01_basics/04_Comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);   //  !=  Not equal to
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);   // in Js we can compare two different types of datatype but in typescript we cant compare
+
+
+// console.log(null > 0);
+// console.log(null >= 0); // Here null value converted as a Zero that's why here output is true 
+// console.log(null == 0);  
+
+
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+
+//== it is conversion 
+console.log("02" == 2);
+
+// ===   it will check  both value and datatypes very strictly
+console.log("02" === 2);
