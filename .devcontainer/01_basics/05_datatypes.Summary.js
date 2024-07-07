49f48dc4bf8829484e68f32ab471a352d7177e15
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-07 20:21:36 +0000

    Stack and heap memory

--- .devcontainer/01_basics/05_datatypes.Summary.js
+++ .devcontainer/01_basics/05_datatypes.Summary.js
@@ -22,16 +22,39 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",    // inside {} is Object
-    age: 22,
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",    // inside {} is Object
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+
+//*****************Stack and Heap Memory*******************/
+
+// Stack -- Primitive Datatype
+// Heap -- Non Primitive datatype
+
+let myName = "Ankush_Kumar"
+let anotherName = myName
+anotherName = "Avinash_kumat"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "avinashkumat95244@gmail.com",
+    upi: "234567876@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+let userTwo = userOne
+userTwo.email = "ankushk1234@gmail.com"
 
-console.log(typeof anotherId);
-
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+console.log(userOne.email);
+console.log(userTwo.email);
