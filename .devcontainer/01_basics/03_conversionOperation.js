a8152bccfd9a922c2fb0e22c473069e3cfe9f07d
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-07 19:12:51 +0000

    operations Postifx and prefix

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -26,7 +26,56 @@
 // "" => false
 // "Ankush" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
- console.log( typeof stringNumber);
+// let stringNumber = String(someNumber)
+//  console.log( typeof stringNumber);
+
+ /************************operation*************************** */
+
+//  let value = 3
+//  let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Ankush"
+// let str3 = str1 +str2
+// console.log(str3);
+
+
+// console.log("2"+2);
+// console.log(2+"2");
+// console.log( 1+"2"+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log("");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2 // that is not good for readability
+
+// let gameCounter = 100
+
+// gameCounter++;
+
+// console.log(gameCounter);
+
+
+// postfix
+
+let x = 3
+const y =x++;
+console.log(x , y);
+
+//prefix
+
+// let x = 3
+// const y = ++x;
+// console.log(x , y);
