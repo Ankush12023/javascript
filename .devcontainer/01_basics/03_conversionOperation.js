27586f001818db246108c3f0d6012f17d2a7578b
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-07 18:29:34 +0000

    conversion of Datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = 33
+
+// console.log( typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+  
+// notes
+// "33" => 33
+// "33abd" => NaN (Not A Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggegIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggegIn)
+// console.log(booleanIsLoggedIn);
+
+// notes
+// 1 => true
+// 0 => false
+// "" => false
+// "Ankush" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log( typeof stringNumber);
