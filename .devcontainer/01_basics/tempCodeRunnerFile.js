336090211bc3b88422032fe765b399e57f2a349a
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-08 18:32:44 +0000

    temp

---
+++ .devcontainer/01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+onst newString = gameName.substring(2, 4)   // In this we always start with positive 
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 6)  // In this we can start with Positive or Negative both
+// console.log(anotherString);
