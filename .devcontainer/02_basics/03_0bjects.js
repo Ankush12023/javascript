49189408c260263d669fb1a6d3f8f4a3bf573114
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-12 10:19:09 +0000

    Objects in Js

---
+++ .devcontainer/02_basics/03_0bjects.js
@@ -0,0 +1,46 @@
+// Objects we can declare in two types:
+// 1-Literal and  2-constructors
+
+
+// Singleton
+// Object.create 
+
+
+// Object Literal 
+const mySym = Symbol("key1")  // Decalre a Symbol
+const jsuser = {
+    name: "Ankush",
+    "full name": "Ankush Kumar",
+    [mySym]: "myKey1",   // symbol syntax  this show in output it is a symbol  but if you are using without [___] then output will comes as a object 
+   // mySym: "mykey1"
+    age: 18,
+    location: "Punjab",
+    email: "avinashkumat95@gmail.com",
+    isloggedIn: false,
+    lastLoginDay: ["Monday","Saturday"]
+}
+//   console.log(jsuser.email);
+//   console.log(jsuser["email"]);
+//   console.log(jsuser["full name"]);
+//   console.log(jsuser[mySym]);
+
+
+  jsuser.email = "Ankushk12023@gmail.com"
+//   Object.freeze(jsuser)   // after freeze there will be No changes in the pbject 
+  jsuser.email = "A12023@gmail.com"
+// console.log(jsuser);
+
+
+
+jsuser.greeting = function(){
+    console.log("Hello JS user");
+}
+jsuser.greetingtwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(jsuser.greeting());
+console.log(jsuser.greetingtwo());
+
+
+// we can access a value in two ways  one is dot -->console.log(jsuser.email)
+                                    //   second is square brackets -->console.log(jsuser[mySym])
