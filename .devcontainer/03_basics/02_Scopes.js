6eb89a5e9cb8c7a628ba6b649d1cfb2eebb4d46d
Ankush kumar <121637268+Ankush12023@users.noreply.github.com>
2024-07-13 09:45:22 +0000

    Scope in javascript

--- .devcontainer/03_basics/02_Scopes.js
+++ .devcontainer/03_basics/02_Scopes.js
@@ -5,9 +5,66 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30
-    console.log("INNER:", a); 
+    // console.log("INNER:", a); 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//   nested Scope
+
+function one (){
+    const username = "Ankush"
+
+    function two() {
+        const website = "Youtube"
+        // console.log(username);
+    }
+    // console.log(website);    // error we can't access this (website) outside the scope 
+    two()
+}
+// one()
+
+
+if (true) {
+    const username = "Ankush"
+    if (username === "Ankush") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);   //   Error
+}
+// console.log(username);     // Error
+
+
+
+//++++++++++++++++++Intersting++++++++++++++++
+
+
+function addone(num) {
+    return num + 1
+}
+addone(5)
+
+//   this is not showing error  we can execute before function 
+
+addone(5)
+function addone(num) {
+    return num + 1
+}
+addone(5)
+
+
+
+// const addtwo = function (num) {
+//     return num + 1
+// }
+// addtwo(5)
+
+    // But this can give Error because when we declare variable  so we can't call function before
+addtwo(5)
+const addtwo = function (num) {
+    return num + 1
+}
